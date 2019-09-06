f2fc6821f392afe20af247acbda31e688b32fead
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-06 20:37:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRANDS = []
-
+ 
   def initialize(brand)
     @brand = brand
   end
@@ -12,9 +10,5 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+
 end
